1ebaf1fe8f78aee1c6541d58883404aa937983ad
tomaswyz <jackywyz@126.com>
2020-10-19 22:15:08 +0800

    Update main.ts

--- example/main.ts
+++ example/main.ts
@@ -1,7 +1,7 @@
 import { serve } from "./deps.ts";
 
 const PORT = 1993;
-const s = serve(`0.0.0.0:${PORT}`);
+//const s = serve(`0.0.0.0:${PORT}`);
 const body = new TextEncoder().encode("Hello World\n");
 
 console.log(`Server started on port ${PORT}`);
